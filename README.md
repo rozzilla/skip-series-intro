dfd2173171e838a527ae12759d59ff3468f96b1f
rozzilla <roberto.bianki@gmail.com>
2025-01-05 10:15:48 +0100

    init
    Signed-off-by: rozzilla <roberto.bianki@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# Skip Series Intro
+
+![logo](./src/images/icon128.png)
+
+## Description 🔭
+
+Automatically skip Netflix series intro.
+
+## Ideas, bug, questions 📫
+
+Feel free to contribute to this open-source project and to open a PR if you have an idea for a new feature.
---
+++ src/scripts/content.js
@@ -0,0 +1,11 @@
+const elements = [
+  '[data-uia="next-episode-seamless-button"]',
+  '[data-uia="interrupt-autoplay-continue"]',
+  ".watch-video--skip-content-button",
+];
+
+setInterval(() => {
+  for (const element of elements) {
+    document.querySelector(element)?.click();
+  }
+}, 200);
